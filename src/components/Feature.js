9588b62ad1bb02890453dbf5b8f69c4915f6376e
Stackbit Code Editor <projects@stackbit.com>
2021-03-26 11:43:50 +0000

    Feature.js: changed

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -35,15 +35,7 @@ export default class Feature extends React.Component {
             <React.Fragment>
                 <div className={classNames('feature', {'maxw-medium': is_vert, 'mx-auto': is_vert, 'py-0': padding_y === 'small', 'py-1': padding_y !== 'small', 'py-sm-3': padding_y === 'large'})}>
                 	<div className="item__content grid items-center">
-                		{has_media && (
-                		<div className={classNames('feature__media', 'my-2', 'cell-12', {'cell-md-4': (is_horiz && has_text) && (media_width === 'thirty-three'), 'cell-md-5': (is_horiz && has_text) && (media_width === 'fourty'), 'cell-md-6': (is_horiz && has_text) && (media_width === 'fifty'), 'cell-md-7': (is_horiz && has_text) && (media_width === 'sixty')})}>
-                			{_.get(feature, 'video_embed_html', null) ? (
-                				htmlToReact(_.get(feature, 'video_embed_html', null))
-                			) : 
-                				<img src={withPrefix(_.get(feature, 'image', null))} alt={_.get(feature, 'image_alt', null)} className={classNames({'mx-auto': align_x === 'center', 'ml-auto': align_x === 'right'})} />
-                			}
-                		</div>
-                		)}
+    
                 		{has_text && (
                 		<div className={classNames('feature__body', 'my-2', 'cell-12', {'cell-md-8': (is_horiz && has_media) && (media_width === 'thirty-three'), 'cell-md-7': (is_horiz && has_media) && (media_width === 'fourty'), 'cell-md-6': (is_horiz && has_media) && (media_width === 'fifty'), 'cell-md-5': (is_horiz && has_media) && (media_width === 'sixty'), 'order-md-first': has_media && (media_pos === 'right'), 'order-first': has_media && (media_pos === 'bottom'), 'text-center': align_x === 'center', 'text-right': align_x === 'right'})}>
                 			{_.get(feature, 'title', null) && (
